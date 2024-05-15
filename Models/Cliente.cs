de30d0b6c8d1072aee17f26aca395e3624b940c3
Matheus Monteiro <41351829+MatheusMonteiro8906@users.noreply.github.com>
2024-05-14 22:11:26 -0300

    update: added Client model and created extract and transaction models

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -4,6 +4,12 @@ namespace RinhaDeDev.Models
 {
     public class Cliente
     {
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "Todo cliente deve ter um limite.")]
+        [Range(0, int.MaxValue, ErrorMessage = "O limite não pode ser negativo.")]
+        public int Limite { get; set; }
+
+        public double SaldoInicial { get; set; } = 0;
     }
 }
---
+++ Models/Extrato.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RinhaDeDev.Models
+{
+    public class Extrato
+    {
+
+    }
+}
